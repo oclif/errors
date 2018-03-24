6528f456cec3e5102e0a255061816da9ba455a89
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-03-24 12:42:03 -0700

    fix: only render if it is a function

--- src/handle.ts
+++ src/handle.ts
@@ -9,7 +9,7 @@ export const handle = (err: any) => {
     if (!err) err = new Error('no error?')
     let stack = clean(err.stack, {pretty: true})
     let message = stack
-    if (err.oclif && err.render) message = err.render()
+    if (err.oclif && typeof err.render === 'function') message = err.render()
     if (message) console.error(message)
     process.exitCode = (err.oclif && err.oclif.exit !== undefined) ? err.oclif.exit : 1
     if (config.errorLogger) {
